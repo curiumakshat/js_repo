22548872343185a75b4bc524c4c520901b3d55d8
curiumakshat <akshat.raj25@st.niituniversity.in>
2025-10-19 11:35:20 +0530

    added data types notes

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //
+
+// Data Types in JavaScript
+
+// 1. Primitive Data Types
+
+// a. Number
+var age = 25; // Integer
+let price = 19.99; // Floating-point number
+
+// alert(3+3) // 6 we are using node js not browser
+console.log(3 + 3); 
+
+let name = "John Doe"; // String
+let age = 30; // Number
+let isStudent = true; // Boolean
+
+//number  => 2 to the power of 53
+//bigint => larger than 2 to the power of 53
+//string => sequence of characters use in "", '', ``
+//boolean => true or false
+//null => empty value stand alone value
+//undefined => value is not assigned
+//symbol => unique identifier
+
+console.log(typeof undefined);
+console.log(typeof null); // object (this is a known quirk in JavaScript)
