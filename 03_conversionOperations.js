403aa3f2d3b3f635f7c449fe852c0b18b78e5589
curiumakshat <akshat.raj25@st.niituniversity.in>
2025-10-19 20:59:24 +0530

    added few more code

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -29,3 +29,25 @@ let someNumber = 123
 let stringValue = String(someNumber) // convert to string
 console.log(typeof stringValue);
 console.log(stringValue); // "123"
+
+// ***************** Operations *****************
+
+let value = 3
+let negValue = -value // unary negation
+console.log(negValue); // -3
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(5%2); // modulus operator gives remainder   
+console.log(2**3); // exponentiation 2 to the power 3
+
+let str1 = "Hello "
+let str2 = "Akshat"
+
+let str3 = str1 + str2 // string concatenation
+console.log(str3); // Hello Akshat
+
+// console.log("1" + 5); // 15
+// console.log
