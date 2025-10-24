4ec9f1410d77b8d215c4403dfc28192c1fca879e
Akshat <akshat.raj25@st.niituniversity.in>
2025-10-25 01:39:49 +0530

    learned strings so adding that

---
+++ 05_strings.js
@@ -0,0 +1,12 @@
+//string interpolation
+
+const name = "Akshat"
+const repoCount = 4
+
+//console.log(`hey! my name is ${name.toUpperCase()}, and i have repocount ${repoCount}`)
+
+const gameName = new String('curium' )
+
+console.log(gameName.length)    
+
+//read some more mdm about string function as it will help you to understand and use those function more effectively 
